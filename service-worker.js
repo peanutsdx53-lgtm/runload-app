ee7f8f75530190f1c2f5bc5fbc1827b98003f51b
peanutsdx53-lgtm <peanuts.dx53@gmail.com>
2026-09-20 15:10:40 +0900

    Bump PWA cache for user-language V1.29

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = "runload-app-current-v128-ui-alignment-v1";
+const CACHE_NAME = "runload-app-current-v129-user-language-v1";
 const RUNLOAD_CACHE_PREFIX = "runload-app-";
 const PRECACHE_URLS = [
   "./app.js",
