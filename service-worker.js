3b32e2e3861831782e4d775edad10bd5d073da24
peanutsdx53-lgtm <peanuts.dx53@gmail.com>
2026-09-22 07:30:00 +0900

    Refresh cache for derived context audit

--- service-worker.js
+++ service-worker.js
@@ -1,5 +1,5 @@
-// Runtime revision: desktop-ui-reaudit-20260922-30
-const CACHE_NAME = "runload-app-current-20260922-30";
+// Runtime revision: desktop-ui-reaudit-20260922-31
+const CACHE_NAME = "runload-app-current-20260922-31";
 const RUNLOAD_CACHE_PREFIX = "runload-app-";
 const PRECACHE_URLS = [
   "./app.js",
