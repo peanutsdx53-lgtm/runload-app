7f973501733c2a37466522ba5ce8dd1b39a382c5
peanutsdx53-lgtm <peanuts.dx53@gmail.com>
2026-09-22 07:31:39 +0900

    Refresh cache after screen architecture re-audit

--- service-worker.js
+++ service-worker.js
@@ -1,5 +1,5 @@
-// Runtime revision: desktop-ui-reaudit-20260922-31
-const CACHE_NAME = "runload-app-current-20260922-31";
+// Runtime revision: desktop-ui-reaudit-20260922-32
+const CACHE_NAME = "runload-app-current-20260922-32";
 const RUNLOAD_CACHE_PREFIX = "runload-app-";
 const PRECACHE_URLS = [
   "./app.js",
