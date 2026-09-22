e78406e3c77247d24e5592f6a0ee16cddcdc557e
peanutsdx53-lgtm <peanuts.dx53@gmail.com>
2026-09-22 10:27:22 +0900

    Refresh cache for desktop typography audit

--- service-worker.js
+++ service-worker.js
@@ -1,5 +1,5 @@
-// Runtime revision: desktop-ui-reaudit-20260922-35
-const CACHE_NAME = "runload-app-current-20260922-35";
+// Runtime revision: desktop-ui-reaudit-20260922-36
+const CACHE_NAME = "runload-app-current-20260922-36";
 const RUNLOAD_CACHE_PREFIX = "runload-app-";
 const PRECACHE_URLS = [
   "./app.js",
