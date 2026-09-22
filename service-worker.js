1e689cfb70374a42578006e27d1e8042f3732657
peanutsdx53-lgtm <peanuts.dx53@gmail.com>
2026-09-22 11:15:39 +0900

    Refresh cache for final desktop visual fixes

--- service-worker.js
+++ service-worker.js
@@ -1,5 +1,5 @@
-// Runtime revision: desktop-ui-reaudit-20260922-36
-const CACHE_NAME = "runload-app-current-20260922-36";
+// Runtime revision: desktop-final-visual-audit-20260922-37
+const CACHE_NAME = "runload-app-current-20260922-37";
 const RUNLOAD_CACHE_PREFIX = "runload-app-";
 const PRECACHE_URLS = [
   "./app.js",
