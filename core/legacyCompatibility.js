7bbe4c4309ae94333a1466b3901fe5f6d9b65d79
peanutsdx53-lgtm <peanuts.dx53@gmail.com>
2026-09-25 13:14:55 +0900

    Centralize legacy compatibility identifiers

---
+++ core/legacyCompatibility.js
@@ -0,0 +1,24 @@
+// Legacy identifiers are kept only to read or clean up data created by older releases.
+// Do not use these values for new records or user-facing labels.
+
+export const LEGACY_LOCAL_DELIVERY_CACHE_PREFIXES = Object.freeze([
+  "runload-journal-",
+  "running-journal-",
+  "runload-new-model-",
+]);
+
+export const LEGACY_TUTORIAL_STORAGE_KEY = "runload.screenTutorial.seen.v1";
+
+export const LEGACY_ROF_J_SEMANTIC_VERSIONS = Object.freeze([
+  "ROF_J_SUZUKI_ARAI_2026_RUNLOAD_V1",
+]);
+
+export const LEGACY_ROF_J_STORAGE_SCHEMA_VERSIONS = Object.freeze([
+  "RUNLOAD_SECOND_PILLAR_ROFJ_STORAGE_V1",
+]);
+
+export const LEGACY_ROF_J_LIFECYCLE_SCHEMA_VERSIONS = Object.freeze([
+  "RUNLOAD_SECOND_PILLAR_ROFJ_LIFECYCLE_V1",
+]);
+
+export const LEGACY_ROF_J_SOURCE_FIELD = "japaneseSourceSha256";
